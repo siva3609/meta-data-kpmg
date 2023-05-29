0489566fec00111132515f4125c50eecb09d5972
siva3609 <134902211+siva3609@users.noreply.github.com>
2023-05-29 10:09:12 +0530

    Update meta.py

--- meta.py
+++ meta.py
@@ -2,23 +2,20 @@ import requests
 import json
 
 def get_aws_instance_metadata():
-    response = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
+    url = 'http://169.254.169.254/latest/meta-data/'
+    response = requests.get(url)
+    metadata = {}
+
     if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+        metadata['instance_id'] = response.text.strip()
+        metadata['availability_zone'] = requests.get(url + 'placement/availability-zone').text.strip()
+        metadata['instance_type'] = requests.get(url + 'instance-type').text.strip()
+        metadata['public_ip'] = requests.get(url + 'public-ipv4').text.strip()
+        metadata['private_ip'] = requests.get(url + 'local-ipv4').text.strip()
 
-# Example usage
-cloud_provider = input("Enter the cloud provider (AWS): ")
+    return metadata
 
-if cloud_provider.lower() == "aws":
-    metadata = get_aws_instance_metadata()
-else:
-    print("Invalid cloud provider.")
-    exit(1)
-
-if metadata:
-    json_output = json.dumps(metadata, indent=4)
-    print(json_output)
-else:
-    print("Failed to retrieve instance metadata.")
+# Usage
+aws_metadata = get_aws_instance_metadata()
+json_output = json.dumps(aws_metadata, indent=4)
+print(json_output)
